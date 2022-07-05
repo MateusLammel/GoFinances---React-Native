6c681f7f4caae2234d9fa67216edd8b22d19793e
Mateus Lammel <lammel.mateus@gmail.com>
2022-07-05 17:44:35 -0300

    Beginning of the project

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Container, Title } from "./styles";
+
+export const Dashboard = () => {
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  );
+};
