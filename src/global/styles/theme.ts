4fc8a1d1f3cc6aec7657266b71e4f80b0b33fd1a
Mateus Lammel <lammel.mateus@gmail.com>
2022-07-07 13:09:03 -0300

    a lot of things of estilization

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -11,6 +11,7 @@ export default {
     shape: "#FFFF",
     title: "#363F5F",
     text: "#969CB2",
+    text_dark: "#00000",
     background: "#F0F2f5",
   },
 
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,67 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { Container, Title } from "./styles";
+import { HighlightCard } from "../../components/HighlightCard";
+import { Title } from "../../components/HighlightCard/styles";
+import {
+  Container,
+  Header,
+  Photo,
+  User,
+  UserGreetings,
+  UserInfo,
+  UserName,
+  UserWrapper,
+  Icon,
+  HighLightCardContainer,
+  Transactions,
+  TitleList,
+} from "./styles";
 
 export const Dashboard = () => {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/92732240?v=4",
+              }}
+            />
+
+            <User>
+              <UserGreetings>Olá,</UserGreetings>
+              <UserName>Mateus</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighLightCardContainer>
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 17.000,00"
+          lastTransaction="Última entrada dia 13"
+        />
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 2.000,00"
+          lastTransaction="Última entrada dia 11"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 15.000,00"
+          lastTransaction="Última entrada dia 11"
+        />
+      </HighLightCardContainer>
+
+      <Transactions>
+        <TitleList>Listagem</TitleList>
+      </Transactions>
     </Container>
   );
 };
